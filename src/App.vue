9ddc416394995903e859ce8f4fb726e163109ea6
francescolazzarin <francesco.lazzarin1996@gmail.com>
2024-02-19 12:21:15 +0100

    <feat>Creazione progetto, con i 3 macro componenti

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppHeader from './components/Header/AppHeader.vue';
+import AppMain from './components/Main/AppMain.vue';
+import AppFooter from './components/Footer/AppFooter.vue';
+
+export default{
+  components:{
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+}
+
+</script>
+
+<template>
+  
+</template>
+<style lang="scss" >
+
+</style>
---
+++ src/components/Footer/AppFooter.vue
@@ -0,0 +1,13 @@
+<script>
+export default{
+   name:'Appfooter' 
+}
+
+</script>
+
+<template>
+  
+</template>
+<style lang="scss" scoped>
+
+</style>
